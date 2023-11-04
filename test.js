263bc927f3f0a05decb149de3efa724ebd04ee11
Deepanshu9650 <119939532+Deepanshu9650@users.noreply.github.com>
2023-11-04 18:23:06 +0000

    discuss about variables

---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Deepanshu")
+console.debug("dg")
+console.log("randi khana bhencho")
---
+++ variables.js
@@ -0,0 +1,6 @@
+// variables = let
+//constants = const
+let email="himuvssakko@gmail.com"
+const password ="12345678"
+password="52525252"
+console.pass(password)
